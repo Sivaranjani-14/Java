80ca6fb55605f95c2c3214a370d3e37c69029745
Sivaranjani S <sivaranjanisubburam@gmail.com>
2026-02-20 20:54:41 +0530

    Added Simple java file

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    
+    // The main method is the entry point where the program starts execution
+    public static void main(String[] args) {
+        
+        // System.out.println prints text to the console and starts a new line
+        System.out.println("Hello, World!");
+    }
+}
